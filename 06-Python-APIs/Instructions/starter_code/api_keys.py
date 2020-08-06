f9d4402b42769a788b61d59a2540cf02949cf576
cce24 <cce24@georgetown.edu>
2020-08-06 17:45:20 -0400

    removing api keys

--- 06-Python-APIs/Instructions/starter_code/api_keys.py
+++
@@ -1,6 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "74cfd4658cb9db9b1790ebd3914d48cb"
-weather_api_key2 = "bc301cb87a3cca643b9c01a0cdb1688b"
-
-# Google API Key
-g_key = "AIzaSyCPPV7XgzwVYc6JWWiV2C82BT-XM-E_kUE"
